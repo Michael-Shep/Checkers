e8e939d2a85324942567f09b4f40b1c427669123
Michael-Shep <mgShepherd1504@gmail.com>
2021-07-21 20:02:19 +0100

    Added functionality for basic moves and commented all functions in the Board file to make clearer

--- checkers/src/Board.js
+++ checkers/src/Board.js
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import { copy } from 'fastest-json-copy';
 
 import GridSquare from "./GridSquare";
 
 const Board = () => {
     const numSquaresInLine = 8;
 
+    /**
+     * Creates the starting grid used for checkers
+     * @returns Grid of values 0, 1 and 2 - 0 means no checker, 1 means black 2 means white
+     */
     const createStartingGrid = () => {
         let grid = [];
         //Create starting configuration for the checkers - 1 represnts black, 2 represents white
@@ -24,18 +29,52 @@ const Board = () => {
     };
 
     const [grid, setGrid] = useState(createStartingGrid());
-    const [firstSelectedPosition, setFirstSelectedPosition] = useState([]);
+    const [selectedPosition, setSelectedPosition] = useState([]);
 
+    /**
+     * Moves a checker from the selected position into the newly selected position
+     * @param {*} newRow The row the checker should be moved to
+     * @param {*} newCol The column the checker should be moved to
+     */
+    const performMove = (newRow, newCol) => {
+        let gridCopy = copy(grid);
+        gridCopy[newRow][newCol] = grid[selectedPosition[0]][selectedPosition[1]];
+        gridCopy[selectedPosition[0]][selectedPosition[1]] = '0';
+        setGrid(gridCopy);
+        setSelectedPosition([]);
+    }
+
+    /**
+     * Handles the click event for one of the grid positions
+     * @param {*} value The checker value currently at that position
+     * @param {*} rowIndex The row of the square that was pressed
+     * @param {*} columnIndex The column of the square that was pressed
+     */
     const onClickHandler = (value, rowIndex, columnIndex) => {
-        //If we have not yet selected a position, make this square the first selected position
-        if (value !== '0' && firstSelectedPosition.length === 0) {
-            setFirstSelectedPosition([rowIndex, columnIndex]);
+        if (value !== '0') {
+            setSelectedPosition([rowIndex, columnIndex]);
+        }
+        //If we already have a checker selected and then select an empty space, see if we can move to that space
+        else if (value === '0' && selectedPosition.length === 2) {
+            //Handle standard movement - moving up or down 1 space diagonally
+            if ((grid[selectedPosition[0]][selectedPosition[1]] === '1' && rowIndex === selectedPosition[0] + 1) || 
+                (grid[selectedPosition[0]][selectedPosition[1]] === '2' && rowIndex === selectedPosition[0] - 1)) {
+                    if (columnIndex === selectedPosition[1] - 1 || columnIndex === selectedPosition[1] + 1) {
+                        performMove(rowIndex, columnIndex);
+                    }
+            }
         }
     };
 
+    /**
+     * Checks whether a given square is the one that has been selected on the grid
+     * @param {*} rowIndex The row of the square to check
+     * @param {*} colIndex The column of the square to check
+     * @returns Boolean representing whether this square is selected or not
+     */
     const isSquareSelected = (rowIndex, colIndex) => {
-        return firstSelectedPosition.length > 0 && firstSelectedPosition[0] === rowIndex 
-               && firstSelectedPosition[1] === colIndex;
+        return selectedPosition.length > 0 && selectedPosition[0] === rowIndex 
+               && selectedPosition[1] === colIndex;
     };
 
     return (
